448198425ddfcf356e604e8d69df9af331a653c5
NatalyAlpeeva <nataha.one@gmail.com>
2024-03-02 09:29:08 +0200

    dz2

---
+++ dz2.js
@@ -0,0 +1,19 @@
+/*var name = prompt ("Вкажіть ваше імя", "18");
+console.log(typeof name, Number(name));
+if() */
+
+var age = prompt("Вкажіть ваш вік", "1" );
+console.log(typeof age, Number(age));
+
+let lastDigit = age % 10;
+if (age >= 10 && age <= 19) {
+	console.log(age + " років")
+} else if (lastDigit == 0) {
+	console.log(age + " років")
+} else if (lastDigit == 1) {
+	console.log(age + " рік")
+} else if (lastDigit >= 2 && lastDigit <= 4) {
+	console.log(age + " роки")
+} else {
+	console.log(age + " років")
+}
